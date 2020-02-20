2f25f92afa3cf107010756e1b58b282b114179e7
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-02-20 22:52:37 +0300

    create 3dglo

---
+++ script/script.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    'use strict';
+
+  const countTimer = (deadline) => {
+    const timerHours = document.getElementById('timer-hours'),
+      timerMinutes = document.getElementById('timer-minutes'),
+      timerSeconds = document.getElementById('timer-seconds');
+
+    const getTimeRemaning = () => {
+        const goodTime = (time) => {
+            if (time > 0 && time < 10) {
+              return `0${time}`;
+            } else if (time <= 0) {
+              return `00`;
+            }
+            return time;
+          },
+          dateStop = new Date(deadline).getTime(),
+          dateNow = new Date().getTime(),
+          timeRemaining = (dateStop - dateNow) / 1000,
+          seconds = goodTime(Math.floor(timeRemaining % 60)),
+          minutes = goodTime(Math.floor((timeRemaining / 60) % 60)),
+          hours = goodTime(Math.floor(timeRemaining / 60 / 60));
+
+        return {
+          hours,
+          minutes,
+          seconds
+        };
+      },
+      updateClock = () => {
+        const timer = getTimeRemaning();
+
+        timerHours.textContent = timer.hours;
+        timerMinutes.textContent = timer.minutes;
+        timerSeconds.textContent = timer.seconds;
+      };
+
+    setInterval(updateClock, 1000);
+  };
+
+  countTimer(`21 february 2020`);
+
+});
