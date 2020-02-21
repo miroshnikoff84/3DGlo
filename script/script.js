7834573a15bedc5f99b7f545e767ba275b1f708c
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-02-22 00:13:51 +0300

    update js

--- script/script.js
+++ script/script.js
@@ -2,44 +2,49 @@ window.addEventListener('DOMContentLoaded', () => {
 
     'use strict';
 
-  const countTimer = (deadline) => {
-    const timerHours = document.getElementById('timer-hours'),
-      timerMinutes = document.getElementById('timer-minutes'),
-      timerSeconds = document.getElementById('timer-seconds');
-// если число однозначное, то подставляем ноль
-    const getTimeRemaning = () => {
-        const goodTime = (time) => {
-            if (time > 0 && time < 10) {
-              return `0${time}`;
-            } else if (time <= 0) {
-              return `00`;
-            }
-            return time;
-          },
-          dateStop = new Date(deadline).getTime(),
-          dateNow = new Date().getTime(),
-          timeRemaining = (dateStop - dateNow) / 1000,
-          seconds = goodTime(Math.floor(timeRemaining % 60)),
-          minutes = goodTime(Math.floor((timeRemaining / 60) % 60)),
-          hours = goodTime(Math.floor(timeRemaining / 60 / 60));
+    const countTimer = (deadline) => {
+        // получаем элементы 
+        const timerHours = document.getElementById('timer-hours'),
+            timerMinutes = document.getElementById('timer-minutes'),
+            timerSeconds = document.getElementById('timer-seconds');
+        // если число однозначное, то подставляем ноль
+        const getTimeRemaning = () => {
+                const goodTime = (time) => {
+                        if (time > 0 && time < 10) {
+                            return `0${time}`;
+                        } else if (time <= 0) {
+                            return `00`;
+                        }
+                        return time;
+                    },
+                    dateStop = new Date(deadline).getTime(),
+                    dateNow = new Date().getTime(),
+                    timeRemaining = (dateStop - dateNow) / 1000,
+                    seconds = goodTime(Math.floor(timeRemaining % 60)),
+                    minutes = goodTime(Math.floor((timeRemaining / 60) % 60)),
+                    hours = goodTime(Math.floor(timeRemaining / 60 / 60));
 
-        return {
-          hours,
-          minutes,
-          seconds
-        };
-      },
-      updateClock = () => {
-        const timer = getTimeRemaning();
+                return {
+                    hours,
+                    minutes,
+                    seconds
+                };
+            },
+                //  устанавлваем интервал и условие его удаления 
+            intervalId = setInterval(() => {
+                const timer = getTimeRemaning();
 
-        timerHours.textContent = timer.hours;
-        timerMinutes.textContent = timer.minutes;
-        timerSeconds.textContent = timer.seconds;
-      };
+                timerHours.textContent = timer.hours;
+                timerMinutes.textContent = timer.minutes;
+                timerSeconds.textContent = timer.seconds;
 
-    setInterval(updateClock, 1000);
-  };
+                if (timer.seconds < 1 && timer.minutes < 1 && timer.hours < 1) {
+                    console.log('Done');
+                    clearInterval(intervalId);
+                }
+            }, 1000);
+    };
 
-  countTimer(`21 february 2020`);
+    countTimer(`24 february 2020`);
 
 });
