2f024ae30d7345cc22795554b9d2b081238875a9
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-02-24 14:32:01 +0300

    create animation

--- script/script.js
+++ script/script.js
@@ -9,27 +9,27 @@ window.addEventListener('DOMContentLoaded', () => {
             timerSeconds = document.getElementById('timer-seconds');
         // если число однозначное, то подставляем ноль
         const getTimeRemaning = () => {
-            const goodTime = (time) => {
-                if (time > 0 && time < 10) {
-                    return `0${time}`;
-                } else if (time <= 0) {
-                    return `00`;
-                }
-                return time;
-            },
-                dateStop = new Date(deadline).getTime(),
-                dateNow = new Date().getTime(),
-                timeRemaining = (dateStop - dateNow) / 1000,
-                seconds = goodTime(Math.floor(timeRemaining % 60)),
-                minutes = goodTime(Math.floor((timeRemaining / 60) % 60)),
-                hours = goodTime(Math.floor(timeRemaining / 60 / 60));
+                const goodTime = (time) => {
+                        if (time > 0 && time < 10) {
+                            return `0${time}`;
+                        } else if (time <= 0) {
+                            return `00`;
+                        }
+                        return time;
+                    },
+                    dateStop = new Date(deadline).getTime(),
+                    dateNow = new Date().getTime(),
+                    timeRemaining = (dateStop - dateNow) / 1000,
+                    seconds = goodTime(Math.floor(timeRemaining % 60)),
+                    minutes = goodTime(Math.floor((timeRemaining / 60) % 60)),
+                    hours = goodTime(Math.floor(timeRemaining / 60 / 60));
 
-            return {
-                hours,
-                minutes,
-                seconds
-            };
-        },
+                return {
+                    hours,
+                    minutes,
+                    seconds
+                };
+            },
             //  устанавлваем интервал и условие его удаления 
             intervalId = setInterval(() => {
                 const timer = getTimeRemaning();
@@ -52,41 +52,133 @@ window.addEventListener('DOMContentLoaded', () => {
             menu = document.querySelector('menu'),
             closeBtn = document.querySelector('.close-btn'),
             menuItems = menu.querySelectorAll('ul>li');
-        
-        const handlerMenu = () => {
-            menu.classList.toggle('active-menu');
-        };
-        
-        btnMenu.addEventListener('click', handlerMenu);
-        
-        closeBtn.addEventListener('click', handlerMenu);
-        for (let i = 0; i < menuItems.length; i++) {
-            menuItems[i].addEventListener('click', handlerMenu);
-        }
+        let stepLeft = 0;
 
-        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+        const handlerMenu = () => {
+            let animId;
+            // Анимация
+            let animate = () => {
+                animId = requestAnimationFrame(animate);
+                if (stepLeft < document.documentElement.clientWidth) {
+                    stepLeft += 60;
+                    menu.style.left = stepLeft + 'px';
+                } else {
+                    cancelAnimationFrame(animId);
+                }
+            };
+
+            // По клику запускает анимацию
+            let clickMenu = () => {
+                if (!menu.style.left) {
+                    animId = requestAnimationFrame(animate);
+                } else {
+                    stepLeft = 0;
+                    menu.removeAttribute('style');
+                    cancelAnimationFrame(animId);
+                }
+            };
+            // Показывает меню при 768px
+            let showMobileMenu = () => {
+                if (!menu.style.transform || menu.style.transform === 'translateX(-100%)') {
+                    menu.style.transform = 'translateX(0)';
+                } else {
+                    menu.style.transform = 'translateX(-100%)';
+
+                }
+            };
+            // Проверка расширения экрана
+            let checkWidth = () => {
+                if (document.documentElement.clientWidth > 768) {
+                    clickMenu();
+                } else {
+                    // cancelAnimationFrame(animId);
+                    showMobileMenu();
+                }
+            };
+
+
+            checkWidth();
+
+        };
+
+
+        btnMenu.addEventListener('click', handlerMenu);
+
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItems.forEach((elem) => {
+            elem.addEventListener('click', handlerMenu);
+        });
 
     };
 
     toggleMenu();
 
-    // popup
-
-    const togglePopUp = () => {
+    // Выезжающий PopUp
+    const slidePopUp = () => {
         const popup = document.querySelector('.popup'),
+            popupContent = document.querySelector('.popup-content'),
             popupBtn = document.querySelectorAll('.popup-btn'),
-            popUpClose = document.querySelector('.popup-close');
-            popupBtn.forEach((elem) => {
-            elem.addEventListener('click', () => {
-            popup.style.display = 'block';
-            });
+            popupClose = document.querySelector('.popup-close');
+
+        let stepLeft = 0;
+
+        const handlerPopUp = () => {
+            let animId;
+            let halfScreen = (document.documentElement.clientWidth * 0.5) - 191;
+            // Анимация
+            let animate = () => {
+                animId = requestAnimationFrame(animate);
+                if (stepLeft < (halfScreen)) {
+                    stepLeft += 40;
+                    popup.style.display = 'block';
+                    popupContent.style.left = stepLeft + 'px';
+                } else {
+                    cancelAnimationFrame(animId);
+                }
+            };
+
+            // По клику запускает анимацию
+            let clickPopUp = () => {
+                if (!popup.style.display) {
+                    animId = requestAnimationFrame(animate);
+                } else {
+                    stepLeft = 0;
+                    popup.removeAttribute('style');
+                    popupContent.removeAttribute('style');
+                    cancelAnimationFrame(animId);
+                }
+            };
+            // Показывает меню при 768px
+            let showMobilePopUp = () => {
+                if (!popup.style.display || popup.style.display === 'none') {
+                    popup.style.display = 'block';
+                } else {
+                    popup.style.display = 'none';
+
+                }
+            };
+            // Проверка расширения экрана
+            let checkWidth = () => {
+                if (document.documentElement.clientWidth > 768) {
+                    clickPopUp();
+                } else {
+                    // cancelAnimationFrame(animId);
+                    showMobilePopUp();
+                }
+            };
+
+
+            checkWidth();
+
+        };
+
+        popupBtn.forEach((elem) => {
+            elem.addEventListener('click', handlerPopUp);
         });
-        popUpClose.addEventListener('click', () => {
-        popup.style.display = 'none';
-    });
+
+        popupClose.addEventListener('click', handlerPopUp);
 
     };
-
-    togglePopUp();
-
+    slidePopUp();
 });
