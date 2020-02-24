915e782bb846d5a5acd270427f60350a3a76e758
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-02-24 14:20:01 +0300

    create lesson18

--- script/script.js
+++ script/script.js
@@ -9,28 +9,28 @@ window.addEventListener('DOMContentLoaded', () => {
             timerSeconds = document.getElementById('timer-seconds');
         // если число однозначное, то подставляем ноль
         const getTimeRemaning = () => {
-                const goodTime = (time) => {
-                        if (time > 0 && time < 10) {
-                            return `0${time}`;
-                        } else if (time <= 0) {
-                            return `00`;
-                        }
-                        return time;
-                    },
-                    dateStop = new Date(deadline).getTime(),
-                    dateNow = new Date().getTime(),
-                    timeRemaining = (dateStop - dateNow) / 1000,
-                    seconds = goodTime(Math.floor(timeRemaining % 60)),
-                    minutes = goodTime(Math.floor((timeRemaining / 60) % 60)),
-                    hours = goodTime(Math.floor(timeRemaining / 60 / 60));
-
-                return {
-                    hours,
-                    minutes,
-                    seconds
-                };
+            const goodTime = (time) => {
+                if (time > 0 && time < 10) {
+                    return `0${time}`;
+                } else if (time <= 0) {
+                    return `00`;
+                }
+                return time;
             },
-                //  устанавлваем интервал и условие его удаления 
+                dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = goodTime(Math.floor(timeRemaining % 60)),
+                minutes = goodTime(Math.floor((timeRemaining / 60) % 60)),
+                hours = goodTime(Math.floor(timeRemaining / 60 / 60));
+
+            return {
+                hours,
+                minutes,
+                seconds
+            };
+        },
+            //  устанавлваем интервал и условие его удаления 
             intervalId = setInterval(() => {
                 const timer = getTimeRemaning();
 
@@ -46,5 +46,47 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     countTimer(`24 february 2020`);
+
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+        
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+        
+        btnMenu.addEventListener('click', handlerMenu);
+        
+        closeBtn.addEventListener('click', handlerMenu);
+        for (let i = 0; i < menuItems.length; i++) {
+            menuItems[i].addEventListener('click', handlerMenu);
+        }
+
+        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+
+    };
+
+    toggleMenu();
+
+    // popup
+
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close');
+            popupBtn.forEach((elem) => {
+            elem.addEventListener('click', () => {
+            popup.style.display = 'block';
+            });
+        });
+        popUpClose.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+
+    };
+
+    togglePopUp();
 
 });
