5aa88905dc26d02f573e4f30ace0e0c9c787bd29
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-02-26 19:46:11 +0300

    create lesson20

--- script/script.js
+++ script/script.js
@@ -9,40 +9,43 @@ window.addEventListener('DOMContentLoaded', () => {
             timerSeconds = document.getElementById('timer-seconds');
         // если число однозначное, то подставляем ноль
         const getTimeRemaning = () => {
-                const goodTime = (time) => {
-                        if (time > 0 && time < 10) {
-                            return `0${time}`;
-                        } else if (time <= 0) {
-                            return `00`;
-                        }
-                        return time;
-                    },
-                    dateStop = new Date(deadline).getTime(),
-                    dateNow = new Date().getTime(),
-                    timeRemaining = (dateStop - dateNow) / 1000,
-                    seconds = goodTime(Math.floor(timeRemaining % 60)),
-                    minutes = goodTime(Math.floor((timeRemaining / 60) % 60)),
-                    hours = goodTime(Math.floor(timeRemaining / 60 / 60));
-
-                return {
-                    hours,
-                    minutes,
-                    seconds
-                };
+            const goodTime = (time) => {
+                if (time > 0 && time < 10) {
+                    return `0${time}`;
+                } else if (time <= 0) {
+                    return `00`;
+                }
+                return time;
             },
-            //  устанавлваем интервал и условие его удаления 
-            intervalId = setInterval(() => {
-                const timer = getTimeRemaning();
+                dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = goodTime(Math.floor(timeRemaining % 60)),
+                minutes = goodTime(Math.floor((timeRemaining / 60) % 60)),
+                hours = goodTime(Math.floor(timeRemaining / 60 / 60));
 
+            return {
+                timeRemaining,
+                hours,
+                minutes,
+                seconds
+            };
+        },
+            updateClock = () => {
+                const timer = getTimeRemaning();
                 timerHours.textContent = timer.hours;
                 timerMinutes.textContent = timer.minutes;
                 timerSeconds.textContent = timer.seconds;
 
-                if (timer.seconds < 1 && timer.minutes < 1 && timer.hours < 1) {
+                if (timer.timeRemaining > 0) {
+                    setTimeout(updateClock, 1000);
+                } else {
                     console.log('Done');
-                    clearInterval(intervalId);
                 }
-            }, 1000);
+            };
+
+        updateClock();
+        
     };
 
     countTimer(`12 marth 2020`);
