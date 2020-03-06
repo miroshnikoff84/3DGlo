943121d64508bd010a2427fcbb3ac2388d37c0b6
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-03-06 19:44:14 +0300

    upd js

--- script/script.js
+++ script/script.js
@@ -436,7 +436,7 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 
 
-        const postData = (body, outputData, errorData) => {
+        const postData = (body) => {
 
             return new Promise((resolve, reject) => {
                 const request = new XMLHttpRequest();
@@ -472,16 +472,20 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     const inputValidation = () => {
-        const body = document.querySelector('body')
+        const body = document.querySelector('body'),
+        inputEmail = document.getElementsByName('user_email');
 
         body.addEventListener('input', (event) => {
             let target = event.target;
             if (target.matches('input[name="user_phone"]')) {
                 target.value = target.value.replace(/[^\+{1}\d\(\)\-]/g, '');
-
+            }
             if (target.matches('input[name="user_name"]') || target.matches('input[name="user_message"]')) {
                 target.value = target.value.replace(/[^а-яА-Я,.!?"';:]/, '');
             }
+
+            inputEmail.forEach((item) => {
+                item.setAttribute('required', '');
         });
 
 
