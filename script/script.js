419815e2e2c7f86f7270d339fbc9250c5eb88a3f
Miroshnikov Anatoliy <miroshnikoff84@mail.ru>
2020-03-06 19:06:17 +0300

    upd js

--- script/script.js
+++ script/script.js
@@ -472,16 +472,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     const inputValidation = () => {
-        const body = document.querySelector('body')
+        const body = document.querySelector('body'),
+        inputEmail = document.getElementsByName('user_email');
 
         body.addEventListener('input', (event) => {
             let target = event.target;
             if (target.matches('input[name="user_phone"]')) {
                 target.value = target.value.replace(/[^\+{1}\d\(\)\-]/g, '');
-
+            }
             if (target.matches('input[name="user_name"]') || target.matches('input[name="user_message"]')) {
                 target.value = target.value.replace(/[^а-яА-Я,.!?"';:]/, '');
             }
+
+            inputEmail.forEach((item) => {
+                item.removeAttribute('required');
+                item.value = '';
+              });
         });
 
 
